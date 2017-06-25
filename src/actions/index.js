8d42702e38225335886753c3a1ce01687f34d020
Edward Ren <eddie.ren.2013@gmail.com>
2017-06-25 12:12:56 -0400

    Created proof-of-concept of working reminders using React.

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import { ADD_REMINDER } from '../constants';
+
+export const addReminder = (text) => {
+  const action = {
+    type: ADD_REMINDER,
+    text
+
+  }
+  console.log('action in addReminder', action);
+  return action;
+}
